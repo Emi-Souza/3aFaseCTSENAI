c0394c7a1f104141b4aaeac0d2f14d5f3dfb1fae
emi-souza <emilly_l_souza@estudante.sesisenai.org.br>
2025-02-10 17:23:49 -0300

    Revisão

---
+++ revizona/src/components/Demo2.jsx
@@ -0,0 +1,14 @@
+import './Demo2.css'
+import { GiFox } from "react-icons/gi";
+
+function Demo2() {
+  return (
+    <div className="container-demos2">
+       <img src="./img/mc.png" alt="" className="img-mc"/>
+       <img src="./img/Demo.png.svg" alt="" />
+       <GiFox />
+    </div>
+  )
+}
+
+export default Demo2
